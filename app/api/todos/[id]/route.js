6e691c36415f44082fabe526294566dd52bd173f
Nicolas <nicolasmiranda36@gmail.com>
2026-02-14 22:14:03 -0300

    entrada en ID invalido

--- app/api/todos/[id]/route.js
+++ app/api/todos/[id]/route.js
@@ -1,6 +1,7 @@
 import { supabase } from "@/lib/supabaseClient" ;
 import { NextResponse } from "next/server";
 
+/*
 export async function DELETE(request, { params }) {
     const id = Number(params.id);
 
@@ -14,4 +15,57 @@ export async function DELETE(request, { params }) {
     }
 
     return NextResponse.json({ message: "Eliminado correctamente" });
+} 
+
+    export async function DELETE(request, context) {
+
+    console.log("Context completo:", context);
+    console.log("Params:", context.params);
+    console.log("ID crudo:", context.params?.id);
+
+    const id = Number(context.params?.id);
+
+    console.log("ID convertido:", id);
+
+    const { error } = await supabase
+        .from("todos")
+        .delete()
+        .eq("id", id);
+
+    if (error) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+
+    return NextResponse.json({ message: "Eliminado correctamente" });
+}*/
+
+export async function DELETE(request, context) {
+    const idParam = context.params?.id;
+
+    console.log("Params:", context.params);
+    console.log("ID", idParam);
+
+    const id = parseInt(idParam);
+
+    if (isNaN(id)) {
+        return NextResponse.json(
+        { error: "ID inválido" },
+        { status: 400 }
+        );
+    }
+
+    const { error } = await supabase
+        .from("todos")
+        .delete()
+        .eq("id", id);
+
+    if (error) {
+        return NextResponse.json(
+            { error: error.message },
+            { status: 500 }
+    );
+    }
+
+    return NextResponse.json({ message: "Eliminado correctamente" });
 }
+
